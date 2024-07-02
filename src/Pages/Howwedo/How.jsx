287f950235c136ef74cec3167c35b297c814b677
Gopi_aadith <ggdesignwaves@gmail.com>
2024-07-02 17:45:40 +0530

    how

--- src/Pages/Howwedo/How.jsx
+++ src/Pages/Howwedo/How.jsx
@@ -4,7 +4,24 @@ import "./How.css";
 const How = () => {
   return (
     <div>
-      <div className="how"></div>
+      <div className="how">
+        <div className="how-content">
+          <h1>How we work Our work flow and tools </h1>
+          <p>Our process always focuses on on-time delivery of quality software to the utmost satisfaction of our customer which has passed rounds of complex testing. To assist us through this process we utilize different tools and workflow to help track our progress.</p>
+        </div>
+        <div className="vector-1">
+          <img src="public/how-1.svg" alt="chumma" />
+        </div>
+      </div>
+      <div className="how">
+        <div className="how-content">
+          <h1>How we work Our work flow and tools </h1>
+          <p>Our process always focuses on on-time delivery of quality software to the utmost satisfaction of our customer which has passed rounds of complex testing. To assist us through this process we utilize different tools and workflow to help track our progress.</p>
+        </div>
+        <div className="vector-2">
+          <img src="public/how-1.svg" alt="chumma" />
+        </div>
+      </div>
     </div>
   )
 }
