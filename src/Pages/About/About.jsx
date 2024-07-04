fe31c86468d8667e403483bee5d0c75f35d4ca17
Gopi_aadith <ggdesignwaves@gmail.com>
2024-07-04 16:44:53 +0530

    about-us

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,14 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import './About.css';
 
-
-
 const contentItems = [
-  { title: "Our Mission", text: "We aim to provide the best services..." },
-  { title: "Our Vision", text: "We envision a world where..." },
-  { title: "Our Values", text: "Integrity, Innovation, and Excellence..." },
-  { title: "Our Team", text: "We have a diverse team of..." },
-  { title: "Our Story", text: "Founded in 2021, we have..." },
+  { 
+    title: "How we work Our work flow and tools", 
+    text: "Our process always focuses on on-time delivery of quality software to the utmost satisfaction of our customer which has passed rounds of complex testing. To assist us through this process we utilize different tools and workflow to help track our progress.", 
+    img: "/aboutus.svg" 
+  },
+  { 
+    title: "1.Initial Discussion", 
+    text: "During the first contact, we want to learn your business goals and product vision and collect the information on the most important functionalities. We also answer the first questions and inform you about the predicted project cost. We review the materials you provide and form a team that will conduct the project analysis..", 
+    img: "/aboutus.svg" 
+  },
+  { 
+    title: "2.Scope Conformation", 
+    text: "During this stage we would like to establish what the plans were made on the initial discussion and bring together structure to the project so that both we and the client can accept. This includes all the technical functionalities and screen or page by page expectations and a technical document is created so that we can agree upon and start our initial work.", 
+    img: "/aboutus.svg" 
+  },
+  { 
+    title: "4.Development", 
+    text: "Multiple teams work on different aspects of the project development and serious testing will be undergoing, both from the technical as well as the functional streams. During this stage we would be releasing the software for the customer to be tested as well so as to take feedback and incorporate them to already done or the upcoming tasks.", 
+    img: "/aboutus.svg" 
+  },
+  { 
+    title: "5.UAT and Testing", 
+    text: "At this stage we would create the UAT server and deploy the built solution into it for the customer to be tested and simultaneously out technical testing team would be doing rigorous load tests, functionality and technical glit testing just to eliminate any issues prevailing in the system. This process continues until the customer agrees and certifies with confidence that the application is ready to be moved to the next stage.", 
+    img: "/aboutus.svg" 
+  },
 ];
 
 const About = () => {
@@ -24,8 +42,13 @@ const About = () => {
   return (
     <div className="about-page">
       <div className="slide">
-        <h2>{contentItems[currentIndex].title}</h2>
-        <p>{contentItems[currentIndex].text}</p>
+        <div className="content">
+          <h2>{contentItems[currentIndex].title}</h2>
+          <p>{contentItems[currentIndex].text}</p>
+        </div>
+        <div className="image-container">
+          <img src={contentItems[currentIndex].img} alt={contentItems[currentIndex].title} />
+        </div>
       </div>
       <div className="indicators">
         {contentItems.map((_, index) => (
