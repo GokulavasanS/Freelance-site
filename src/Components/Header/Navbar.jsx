70f33003a7203b6a557c2b3fb91c2a975a113b3c
Gopi_aadith <ggdesignwaves@gmail.com>
2024-07-05 16:37:09 +0530

    navbar

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -13,20 +13,18 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar">
-        <div className="small">
-        <div className="logo-title">
+        {/* <div className="logo-title">
           <img src={Logo} alt="Logo" className="image" />
           <h2 className="title">BRANDOM</h2>
-        </div>
+        </div> */}
         <ul className="nav-links">
-          <li className="home">HOME</li>
-          <li className="about">HOW WE DO</li>
-          <li className="services">SERVICES</li>
-          <li className="contact">CONTACT US</li>
+          <li className="link">HOME</li>
+          <li className="link">HOW WE DO</li>
+          <li className="link">SERVICES</li>
+          <li className="link">CONTACT US</li>
           <div className="button">PORTFOLIO</div>
         </ul>
         {/* <ThemeToggle onThemeChange={handleThemeChange} /> */}
-        </div>
       </nav>
     </div>
   );
